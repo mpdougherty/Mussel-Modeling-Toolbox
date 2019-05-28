aa8f47e8ecf13bf1a181b7dff093ef4fd76b6c4d
Michael P. Dougherty <Michael.P.Dougherty@usace.army.mil>
2019-05-28 15:41:56 -0500

    Added the load_resri function

--- utils.R
+++ utils.R
@@ -23,3 +23,41 @@ load_packages <- function(need_pkgs) {
   # Load all needed packages
   lapply(need_pkgs, require, character.only = TRUE)
 }
+
+#' @title Load resri package
+#' 
+#' @description Installs the required R packages for the ArcGIS FluvialGeomorph
+#'     toolbox. 
+#' 
+#' @export
+#'  
+#' @return Installs the required ArcGIS FluvialGeomorph R packages. 
+#' 
+#' @details This function installs the \code{RegionalCurve} R package from 
+#'     GitHub and the \code{fgm} R package from a local source tarball. 
+#' 
+load_resri <- function() {
+  # Install devtools
+  if (!require("devtools")) { 
+    install.packages("devtools", dependencies = TRUE)
+    if ("devtools" %in% rownames(installed.packages()) == TRUE) {
+      message("The `devtools` package was installed.")
+    }
+  }
+  
+  # Install `resri` from github
+  if (!require("resri")) {
+    devtools::install_github(repo = "mpdougherty/resri",
+                             force = TRUE,
+                             upgrade = TRUE,
+                             dependencies = TRUE,
+                             options(install.packages.check.source = "no"))
+    if ("resri" %in% rownames(installed.packages()) == TRUE) {
+      message("The `resri` package was installed.")
+    }
+  } else {
+    if ("resri" %in% rownames(installed.packages()) == TRUE) {
+      message("The `resri` package was already installed.")
+    }
+  }
+}
